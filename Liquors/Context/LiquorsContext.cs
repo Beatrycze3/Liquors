056be942b1214aa94a731efd915c6a6404525360
Milena Pokorska <mp.pokorska@gmail.com>
2017-11-27 15:23:33 +0100

    Create config, context and Home controller

---
+++ Liquors/Context/LiquorsContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Liquors.Context
+{
+    public class LiquorsContext : DbContext
+    {
+        public LiquorsContext()
+        { }
+
+        public LiquorsContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- Liquors/Program.cs
+++ Liquors/Program.cs
@@ -17,9 +17,8 @@ namespace Liquors
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args)
+                            .UseStartup<Startup>().UseKestrel()
+                            .Build();
     }
 }
