e56458b37c05c2a24ce9741f8a8d9314f5c079db
Milena <mp.pokorska@gmail.com>
2017-11-30 19:31:13 +0100

    Add WineType model

--- Liquors/Model/Wine.cs
+++ Liquors/Model/Wine.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Liquors.Model.WineModel;
 
 namespace Liquors.Model
 {
     public class Wine
     {
         public int Id { get; set; }
-        public string Type { get; set; }
+        public WineType Type { get; set; }
         public string Country { get; set; }
         public string Region { get; set; }
         public string Winery { get; set; }
 
         [ForeignKey("Id")]
         public Alcohol Alcohol { get; set; }
-
     }
+   
 }
 
---
+++ Liquors/Model/WineModel/WineType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Liquors.Model.WineModel
+{
+    public class WineType
+    {
+        public int Id { get; set; }
+        public Type Type { get; set; }
+    }
+
+    public enum Type
+    {
+        Red,
+        White,
+        Rose,
+        Sparkling,
+        Dessert,
+        Port
+    }
+
+
+}
