c434ecf2dfe66795255b0f63ba95db82f0adb1fa
Milena Pokorska <mp.pokorska@gmail.com>
2017-11-30 15:06:01 +0100

    create alcohol model

---
+++ Liquors/Model/Alcohol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Liquors.Model
+{
+    public class Alcohol
+    {
+        public int Id { get; set; }
+
+        public Wine Wine { get; set; }
+    }
+}
---
+++ Liquors/Model/Vintage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Liquors.Model
+{
+    public class Vintage
+    {
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+    }
+}
